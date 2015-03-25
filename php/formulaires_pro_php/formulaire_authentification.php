7edfd25be91c0a6add4ffa5be057f0065e46fd8b
cocow <xrlcoco@gmail.com>
2015-03-25 17:35:49 +0100

    merci dom

--- php/formulaires_pro_php/formulaire_authentification.php
+++ php/formulaires_pro_php/formulaire_authentification.php
@@ -1,20 +1,29 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<title>Formulaire authentification professionnel</title>
+		<link href="../../css/authentification.css" rel="stylesheet" />
 	</head>
 	<body>
 		<form action='authentification.php' method='POST'>
+		
 		<fieldset>
+			<div id="authent">
 		Adresse mail : 
 			<input type='email' name='email_usager'/>
 		</br>
+	</br>
 		Mot de passe : 
 			<input type='password' name='pw_usager'/>
 		<br/>
+	</br>
 			<input type='submit' value='S&#39;identifier'/>
 		</br>
+		</div>
 		</fieldset>
+		
 		</form>
+		<img src="../../css/images_css/authentification.PNG" alt="pile de livres"/>
 	</body>
 </html>
