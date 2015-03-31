121416d56d618b0118b52e46187ffe1959c7c388
marieguyot <maia.mulot@laposte.net>
2015-03-31 09:55:38 +0200

    essai ultime (j'espère)
    pour le isset de la recherche appartenant aux invariants

--- php/invariants/recherche.php
+++ php/invariants/recherche.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+	
+	
+
+<head>
+</head>
+
+<body>
 <!-- Encart gauche avec la recherche -->
 <div class="encart_recherche"> <!-- pourquoi mettre une class ici ? Il n'y aura pas plusieurs encart_recherche dans la même page ? -->
 <aside>
@@ -7,10 +16,14 @@
 <form action="../recherche_php/interrogation_rech_simple.php" method="POST"> <!--il manque le fichier php bien sûr-->
 <input type ="text" name="recherche_simple"/>
 <br/>
-<input type ="submit" value="Chercher"/>
+<input type ="submit" name="submit" value="Chercher"/>
 <input type ="reset" value="Effacer"/>
 </form>
 </div>
 <h4 id="recherche"><a href="../recherche_php/formulaire_recherche_avancee.php"> Recherche avancée<a></h4>
 </aside>
 </div>
+
+
+</body>
+</html>
